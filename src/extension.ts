580af10d81546b50e86114460fa2afc52777ead2
Brian Ignacio <brian.ignacio@espressif.com>
2024-07-29 19:12:27 +0800

    move appendIdfAndToolsToPath to computed py env

--- src/extension.ts
+++ src/extension.ts
@@ -869,7 +869,7 @@ export async function activate(context: vscode.ExtensionContext) {
   registerIDFCommand("espIdf.customTask", async () => {
     try {
       const customTask = new CustomTask(workspaceRoot);
-      customTask.addCustomTask(CustomTaskType.Custom);
+      await customTask.addCustomTask(CustomTaskType.Custom);
       await TaskManager.runTasks();
     } catch (error) {
       const errMsg =
@@ -2990,7 +2990,7 @@ export async function activate(context: vscode.ExtensionContext) {
         const elfFilePath = path.join(buildDirPath, `${projectName}.elf`);
         const wsPort = idfConf.readParameter("idf.wssPort", workspaceRoot);
         const idfVersion = await utils.getEspIdfFromCMake(idfPath);
-        let sdkMonitorBaudRate: string = utils.getMonitorBaudRate(
+        let sdkMonitorBaudRate: string = await utils.getMonitorBaudRate(
           workspaceRoot
         );
         const noReset = idfConf.readParameter(
@@ -3036,8 +3036,8 @@ export async function activate(context: vscode.ExtensionContext) {
         wsServer = new WSServer(wsPort);
         wsServer.start();
         wsServer
-          .on("started", () => {
-            monitor.start();
+          .on("started", async () => {
+            await monitor.start();
           })
           .on("core-dump-detected", async (resp) => {
             const notificationMode = idfConf.readParameter(
@@ -3185,7 +3185,9 @@ export async function activate(context: vscode.ExtensionContext) {
       if (!args) {
         // try to get the partition table name from sdkconfig and if not found create one
         try {
-          const sdkconfigFilePath = utils.getSDKConfigFilePath(workspaceRoot);
+          const sdkconfigFilePath = await utils.getSDKConfigFilePath(
+            workspaceRoot
+          );
           const sdkconfigFileExists = await pathExists(sdkconfigFilePath);
           if (!sdkconfigFileExists) {
             const buildProject = await vscode.window.showInformationMessage(
@@ -3199,7 +3201,7 @@ export async function activate(context: vscode.ExtensionContext) {
             }
             return;
           }
-          const isCustomPartitionTableEnabled = utils.getConfigValueFromSDKConfig(
+          const isCustomPartitionTableEnabled = await utils.getConfigValueFromSDKConfig(
             "CONFIG_PARTITION_TABLE_CUSTOM",
             workspaceRoot
           );
@@ -3232,7 +3234,7 @@ export async function activate(context: vscode.ExtensionContext) {
             }
           }
 
-          let partitionTableFilePath = utils.getConfigValueFromSDKConfig(
+          let partitionTableFilePath = await utils.getConfigValueFromSDKConfig(
             "CONFIG_PARTITION_TABLE_CUSTOM_FILENAME",
             workspaceRoot
           );
@@ -3714,6 +3716,13 @@ async function createCmdsStatusBarItems() {
   if (!enableStatusBar) {
     return {};
   }
+  if (!workspaceRoot) {
+    workspaceRoot =
+      vscode.workspace.workspaceFolders &&
+      vscode.workspace.workspaceFolders.length
+        ? vscode.workspace.workspaceFolders[0].uri
+        : undefined;
+  }
   const port = idfConf.readParameter("idf.port", workspaceRoot) as string;
   let idfTarget = idfConf.readParameter("idf.adapterTargetName", workspaceRoot);
   let flashType = idfConf.readParameter(
@@ -3954,7 +3963,7 @@ function createQemuMonitor(
       monitorTerminal.sendText(ESP.CTRL_RBRACKET);
       monitorTerminal.sendText(`exit`);
     }
-    monitorTerminal = idfMonitor.start();
+    monitorTerminal = await idfMonitor.start();
   });
 }
 
@@ -4099,8 +4108,8 @@ async function startFlashing(
 }
 
 function createIdfTerminal() {
-  PreCheck.perform([webIdeCheck, openFolderCheck], () => {
-    const modifiedEnv = utils.appendIdfAndToolsToPath(workspaceRoot);
+  PreCheck.perform([webIdeCheck, openFolderCheck], async () => {
+    const modifiedEnv = await utils.appendIdfAndToolsToPath(workspaceRoot);
     const espIdfTerminal = vscode.window.createTerminal({
       name: "ESP-IDF Terminal",
       env: modifiedEnv,
@@ -4146,7 +4155,7 @@ async function createIdfMonitor(
     monitorTerminal.sendText(ESP.CTRL_RBRACKET);
     monitorTerminal.sendText(`exit`);
   }
-  monitorTerminal = idfMonitor.start();
+  monitorTerminal = await idfMonitor.start();
   if (noReset) {
     const idfPath = idfConf.readParameter(
       "idf.espIdfPath",
